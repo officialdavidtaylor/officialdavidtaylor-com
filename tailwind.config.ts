e7d2bbb21e83de39ee82989771cd6db1ab31136b
David Taylor <officialdavidtaylor@gmail.com>
2025-01-03 10:39:32 -0800

    add google fonts, extend tailwindcss config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,28 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      // extend existing font utilities
+      sans: ['"Atkinson Hyperlegible"', ...defaultTheme.fontFamily.sans],
+      serif: ['"Noto Serif"', ...defaultTheme.fontFamily.serif],
+      mono: ['"Fira Code"', ...defaultTheme.fontFamily.mono],
+
+      // create new font utilities
+      display: [
+        'Geologica',
+        'ui-sans-serif',
+        'system-ui',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      'casual-heading': ['"Permanent Marker"'],
+    },
   },
   plugins: [],
 };
