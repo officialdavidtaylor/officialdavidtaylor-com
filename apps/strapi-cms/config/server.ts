03df8a6ba5eeff80185004b746488a434a30a745
David Taylor <officialdavidtaylor@gmail.com>
2026-03-23 09:32:08 -0700

    feat(cms): add strapi content workspace

---
+++ apps/strapi-cms/config/server.ts
@@ -0,0 +1,9 @@
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', undefined),
+  proxy: env.bool('TRUST_PROXY', false),
+  app: {
+    keys: env.array('APP_KEYS'),
+  },
+});
